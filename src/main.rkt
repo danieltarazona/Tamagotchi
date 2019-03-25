bd7f8e4a289bbf15aedb4d73a31d1d286f4b2dc0
GoVirus <codeapps@aol.com>
2019-03-25 17:10:48 -0500

    Mouse Event Handler

--- src/main.rkt
+++ src/main.rkt
@@ -4,27 +4,29 @@
 (require 2htdp/image)
 
 
-(define (create-UFO-scene height)
-	(underlay/xy (rectangle 720 720 "solid" "white") 100 height title1))
+(define (create-tamagotchi-scene height)
+  (underlay/xy (rectangle 720 720 "solid" "white") 100 height title1)
+)
 
-(define title1 
-	(above (text "Tamagotchi" 97 "purple")
-          (bitmap "img/panda.jpg")
-          (overlay (rectangle 100 80 "outline" "black")(text "Play" 20 "red"))))
+(define title1
+  (above (text "Tamagotchi" 97 "purple")
+         (bitmap "img/panda.jpg")
+         (overlay (rectangle 100 80 "outline" "black")(text "Play" 20 "red"))
+  )
+)
 
-(define (WorldChange w)
-  (cond
-    [(mouse-event=? "enter")(world-
 
-(define (click w mouse)
-  (cond
-   [(mouse-event=? "enter")
+(define (changeWorldMouse w x y me)
+  (cond [(mouse-event=? me 'left-down) (+ 1 w)]
+  )
+)
+
+
 
 (big-bang 0
-  (on-tick add1 60 1)
-  (define (
-  (to-draw create-UFO-scene 720 720))
+  (on-tick add1)
+  (to-draw create-tamagotchi-scene 720 720)
+  (on-mouse changeWorldMouse)
+)
   
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
   
