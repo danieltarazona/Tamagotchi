ae3f613cd1791e9e3f3496aec9ada75edfb465db
unknown <codeapps@aol.com>
2019-04-12 20:55:13 -0500

    Distribute Script running first 1.0 RELEASE

---
+++ src/scripts/createdir2.sh
@@ -0,0 +1,5 @@
+echo "Creating the executable..."
+cd $USERPROFILE/tamagotchi/src
+echo "Building..."
+rm -rf build
+mkdir $USERPROFILE/tamagotchi/src/build
--- src/scripts/distribute.sh
+++ src/scripts/distribute.sh
@@ -1,10 +1,17 @@
 echo "Creating the executable..."
-raco exe main.rkt
+cd $USERPROFILE/tamagotchi/src
+echo "Building..."
+rm -rf build
+mkdir $USERPROFILE/tamagotchi/src/build
+raco exe --gui -v $USERPROFILE/tamagotchi/src/main.rkt
+mv $USERPROFILE/tamagotchi/src/main.exe build/
+cd $USERPROFILE/tamagotchi/src/build
 echo "Making a standalone distribution..."
-raco distribute pandasushi main
-rm main
+raco distribute pandasushi main.exe
+# rm $USERPROFILE/tamagotchi/src/build/main.exe
+mv main.exe PandaSushi.exe
 echo "Making a zipfile for the distribution..."
-zip -r pandasushi pandasushi
+zip -r PandaSushi pandasushi
 rm -rf pandasushi
 if [ ! -f pandasushi.zip ]; then
     echo "Failed!"
