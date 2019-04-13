3bc4e19875111beb7555bb231b2d9c17aee0923c
unknown <codeapps@aol.com>
2019-04-12 22:06:52 -0500

    Zip RELEASE working with Assets

--- src/scripts/distribute.sh
+++ src/scripts/distribute.sh
@@ -7,12 +7,13 @@ raco exe --gui -v $USERPROFILE/tamagotchi/src/main.rkt
 mv $USERPROFILE/tamagotchi/src/main.exe $USERPROFILE/tamagotchi/build/
 cd $USERPROFILE/tamagotchi/build
 echo "Making a standalone distribution..."
-raco distribute pandasushi main.exe
+raco distribute PandaSushi main.exe
 # rm $USERPROFILE/tamagotchi/src/build/main.exe
 mv main.exe PandaSushi.exe
 echo "Making a zipfile for the distribution..."
-zip -r PandaSushi pandasushi
-rm -rf pandasushi
+cp -r $USERPROFILE/tamagotchi/src/assets $USERPROFILE/tamagotchi/build/PandaSushi
+zip -r PandaSushi PandaSushi 
+rm -rf PandaSushi
 rm PandaSushi.exe
 if [ ! -f pandasushi.zip ]; then
     echo "Failed!"
